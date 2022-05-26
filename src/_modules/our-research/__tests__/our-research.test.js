800d544c158255aa70a1264fad9c32ad08519763
Hadi Lee <hadi@mintable.com>
2022-05-26 21:53:32 +0800

    WIP explore our research page

---
+++ src/_modules/our-research/__tests__/our-research.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import OurResearch from '../our-research';
+
+describe('OurResearch View', function() {
+
+  beforeEach(() => {
+    this.ourResearch = new OurResearch();
+  });
+
+  it('Should run a few assertions', () => {
+    expect(this.ourResearch);
+  });
+
+});
---
+++ src/_modules/our-research/our-research.js
@@ -0,0 +1,8 @@
+'use strict';
+
+export default class OurResearch {
+  constructor() {
+    this.name = 'our-research';
+    console.log('%s module', this.name.toLowerCase());
+  }
+}
