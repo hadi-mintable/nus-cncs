4563ba6a8af2be053e294841caa39d976583bf30
Hadi Lee <hadi@mintable.com>
2022-05-31 15:56:50 +0800

    Handled articles listing for mobile and search s parameter

--- src/_modules/our-research/our-research.js
+++ src/_modules/our-research/our-research.js
@@ -1,8 +1,23 @@
-'use strict';
+"use strict";
 
+import $ from "jquery";
 export default class OurResearch {
   constructor() {
-    this.name = 'our-research';
-    console.log('%s module', this.name.toLowerCase());
+    const $researchAreas = $(".our-research__wrapper li");
+    const $chosenEle = $(".our-research__chosen");
+    const $chosenPara = $(".our-research__chosen > p");
+
+    $researchAreas.on("click", e => {
+      e.preventDefault();
+
+      $chosenEle.removeClass("is-open");
+
+      const bodyText = $(e.currentTarget).data("body");
+      $chosenPara.text(bodyText);
+
+      setTimeout(() => {
+        $chosenEle.addClass("is-open");
+      }, 500);
+    });
   }
 }
--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -8,6 +8,7 @@ import "regenerator-runtime/runtime";
 import $ from "jquery";
 import { HeroBanner } from "../_modules/hero-banner/hero-banner";
 import ArticlesListing from "../_modules/articles-listing/articles-listing";
+import OurResearch from "../_modules/our-research/our-research";
 
 $(() => {
   if ($(".hero-banner").length) {
@@ -16,5 +17,9 @@ $(() => {
 
   if ($(".articles-listing").length) {
     new ArticlesListing();
+  }
+
+  if ($(".our-research").length) {
+    new OurResearch();
   }
 });
